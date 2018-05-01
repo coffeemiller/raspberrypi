262c9945fb3d535e8f641fa214817543e73919bf
juunini <juuni.k.qr@gmail.com>
2018-05-01 13:33:45 +0900

    Add files via upload

--- C/12.FLAME.c
+++ C/12.FLAME.c
@@ -9,7 +9,9 @@ int timestamp(void){    // 시간을 출력하기 위한 모듈
     struct tm *tm;
     time(&time_now);
     tm = localtime(&time_now);
-    return printf("%d-%02d-%02d %02d:%02d:%02d ", tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+    return (printf("%d-%02d-%02d %02d:%02d:%02d ", 
+    tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, 
+    tm->tm_hour, tm->tm_min, tm->tm_sec));
 }
 
 int main(void) {
